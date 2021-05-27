dd13a930f2d476717900f3c0513802a6238d579c
S Sanjay <72385637+sanjay445@users.noreply.github.com>
2021-05-27 10:15:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Inputstudent from './components/Inputstudent'
+import Liststudent from './components/Liststudent'
+function App() {
+  return (
+    <div>
+      <Inputstudent/>
+      <Liststudent/>
+    </div>
+  );
+}
+
+export default App;
